db17ffa69103b8c5a20251623682dde21fe8b859
Krishna Prasad <krishnaprasad@Krishnas-Mac-mini.local>
2016-12-23 12:51:54 -0800

    secondcommentedited

--- hello/AppDelegate.m
+++ hello/AppDelegate.m
@@ -16,8 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
-    return YES;
+      return YES;
 }
 
 
@@ -32,12 +31,12 @@
 
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+    
 }
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+   
 }
 
 
